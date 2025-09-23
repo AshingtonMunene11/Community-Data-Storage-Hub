dfa4cc066c6220dae8d47876efe38d6afd98d0f6
Ashington Munene <munenefrank11@gmail.com>
2025-09-23 23:10:42 +0300

    update for dev3

--- server/schemas/allocation_schema.py
+++ server/schemas/allocation_schema.py
@@ -2,11 +2,16 @@ from marshmallow import Schema, fields
 from server.schemas.upload_schema import UploadSchema
 from server.schemas.storage_node_schema import StorageNodeSchema
 
-class AllocationSchema(Schema):
-    id = fields.Int(dump_only=True)
-    upload_id = fields.Int(required=True)
-    storage_node_id = fields.Int(required=True)
-    allocated_size = fields.Float(required=True)
+
+class AllocationSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Allocation
+        load_instance = True
+
+    id = ma.auto_field()
+    upload_id = ma.auto_field()
+    storage_node_id = ma.auto_field()
+    allocated_size = ma.auto_field()
 
     upload = fields.Nested(UploadSchema, dump_only=True)
     storage_node = fields.Nested(StorageNodeSchema, dump_only=True)
