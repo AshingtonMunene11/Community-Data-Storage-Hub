03348cef584e3fbbfe1c89712b89515b18bba004
Ashington Munene <munenefrank11@gmail.com>
2025-09-23 20:12:00 +0300

    second redo of dev2 matching auth

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,2 +1,4 @@
-from .upload import Upload
-from .allocation import Allocation
+from server.models.user import User
+from server.models.upload import Upload
+from server.models.allocation import Allocation
+from server.models.storage_node import StorageNode
--- server/models/upload.py
+++ server/models/upload.py
@@ -2,16 +2,27 @@ from datetime import datetime
 from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from server.extensions import db
+from server.models.user import User
+
 
 
 class Upload(db.Model):
     __tablename__ = 'uploads'
 
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(255), nullable=False)
+    size = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # Foreign key from Mike`s User model`
+
+
+    """
     id = Column(Integer, primary_key=True)
     filename = Column(String(255), nullable=False)
     size = Column(Float, nullable=False)  # Size in MB or KB
     timestamp = Column(DateTime, default=datetime.utcnow)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    """
 
     
     user = relationship('User', backref='uploads')
