0ae1bb6770fe898e2b7558c1dd1338f0762001b3
Ashington Munene <munenefrank11@gmail.com>
2025-09-24 01:33:47 +0300

    fixes migrations, re did the virtual

--- server/models/allocation.py
+++ server/models/allocation.py
@@ -1,6 +1,3 @@
-from sqlalchemy import Column, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
-from .storage_node import StorageNode
 from server.extensions import db
 
 class Allocation(db.Model):
@@ -11,17 +8,6 @@ class Allocation(db.Model):
     storage_node_id = db.Column(db.Integer, db.ForeignKey('storage_nodes.id'), nullable=False)
     allocated_size = db.Column(db.Float, nullable=False)
 
-
-    """
-    id = Column(Integer, primary_key=True)
-    upload_id = Column(Integer, ForeignKey('uploads.id'), nullable=False)
-    storage_node_id = Column(Integer, ForeignKey('storage_nodes.id'), nullable=False)
-    allocated_space = Column(Float, nullable=False)  # Space in MB or GB
-
-    storage_node = relationship('StorageNode', backref='allocations')
-    """
-
     upload = db.relationship('Upload', backref='allocations')
     storage_node = db.relationship('StorageNode', backref='allocations')
-    node = db.relationship('StorageNode', backref='allocations')
 
