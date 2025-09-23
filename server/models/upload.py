de15ef3c2898e7a194e5de739f22600f7561aa28
Ashington Munene <munenefrank11@gmail.com>
2025-09-24 01:33:47 +0300

    fixes migrations, re did the virtual

--- server/models/upload.py
+++ server/models/upload.py
@@ -1,10 +1,6 @@
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
 from server.extensions import db
 from server.models.user import User
-
-
 
 class Upload(db.Model):
     __tablename__ = 'uploads'
@@ -13,17 +9,7 @@ class Upload(db.Model):
     filename = db.Column(db.String(255), nullable=False)
     size = db.Column(db.Float, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # Foreign key from Mike`s User model`
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-
-    """
-    id = Column(Integer, primary_key=True)
-    filename = Column(String(255), nullable=False)
-    size = Column(Float, nullable=False)  # Size in MB or KB
-    timestamp = Column(DateTime, default=datetime.utcnow)
-    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    """
-
-    
-    user = relationship('User', backref='uploads')
-    allocations = relationship('Allocation', backref='upload', cascade='all, delete-orphan')
+    user = db.relationship('User', backref='uploads')
+    allocations = db.relationship('Allocation', backref='upload', cascade='all, delete-orphan')
