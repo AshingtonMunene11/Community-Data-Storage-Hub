f7a3aa575e9d90498aa630bf25172b9dc1b1bbbe
Ashington Munene <munenefrank11@gmail.com>
2025-09-23 22:29:59 +0300

    first updated models, still working on more

---
+++ server/routes/storage_nodes.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, request, jsonify
+from server.models.storage_node import StorageNode
+from server.schemas.storage_node_schema import StorageNodeSchema
+from server.extensions import db
+
+storage_nodes_bp = Blueprint('storage_nodes_bp', __name__, url_prefix='/api/nodes')
+node_schema = StorageNodeSchema()
+nodes_schema = StorageNodeSchema(many=True)
+
+# GET all nodes
+@storage_nodes_bp.route('/', methods=['GET'])
+def get_nodes():
+    nodes = StorageNode.query.all()
+    return jsonify(nodes_schema.dump(nodes)), 200
+
+# POST create node
+@storage_nodes_bp.route('/', methods=['POST'])
+def create_node():
+    data = request.get_json()
+    new_node = node_schema.load(data)
+    db.session.add(new_node)
+    db.session.commit()
+    return jsonify(node_schema.dump(new_node)), 201
+
+# DELETE node by ID
+@storage_nodes_bp.route('/<int:id>', methods=['DELETE'])
+def delete_node(id):
+    node = StorageNode.query.get_or_404(id)
+    db.session.delete(node)
+    db.session.commit()
+    return jsonify({"message": f"Node {id} deleted"}), 200
