92df5318152905831f420381b28295f2b82098f6
Ashington Munene <munenefrank11@gmail.com>
2025-09-23 23:34:42 +0300

    updates required models, next try initiating testing

--- server/routes/storage_nodes.py
+++ server/routes/storage_nodes.py
@@ -29,3 +29,25 @@ def delete_node(id):
     db.session.delete(node)
     db.session.commit()
     return jsonify({"message": f"Node {id} deleted"}), 200
+
+# GET node summary dashboard
+@storage_nodes_bp.route('/summary', methods=['GET'])
+def node_summary():
+    nodes = StorageNode.query.all()
+    summary = []
+
+    for node in nodes:
+        used = sum(a.allocated_size for a in node.allocations)
+        remaining = node.capacity - used
+        summary.append({
+            "id": node.id,
+            "name": node.name,
+            "location": node.location,
+            "status": node.status,
+            "capacity": node.capacity,
+            "used": used,
+            "remaining": remaining,
+            "allocations": len(node.allocations)
+        })
+
+    return jsonify(summary), 200
---
+++ server/seeds/__init__.py
@@ -0,0 +1,4 @@
+from .seed_storage_nodes import seed_storage_nodes
+
+def run_all_seeders():
+    seed_storage_nodes()
