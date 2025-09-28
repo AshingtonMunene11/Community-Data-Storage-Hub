3c9ad34ae5a333dee06f27b078c09ec5c998c5b9
Ashington Munene <munenefrank11@gmail.com>
2025-09-29 00:33:29 +0300

    connect the deployed backend URL to frontend

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  baseURL: "https://community-data-storage-hub-7.onrender.com", 
   headers: {
     "Content-Type": "application/json",
   },
